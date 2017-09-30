b708ec1d1010b47758b617f9ba3109946823dc0d
Luke Hardin <luke.hardin@mavs.uta.edu>
2017-09-30 18:14:30 -0500

    merge issue

--- board.py
+++ board.py
@@ -8,18 +8,19 @@ class Board:
     def set_black_pieces(self):
         i = 0
         j = 0
-        k = 0
+        k = 12
         for i in range(3):
             for j in range(4):
                 if i == 0 or i == 2:
-                    x = 1+(2*j)
-                    y = 7-i
-                    self.pieces[(x, y)] = (Checker(k, "black", 1+(2*j), 7-i))
+                    x = 1+(j*2)
+                    y = i
+                    self.pieces[(x, y)] = (Checker(k, "black", x, y))
                     k += 1
                 if i == 1:
-                    self.pieces.append(Checker(k, "black", (2*j), 7-i))
+                    x = (2*j)
+                    y = (7-i)
+                    self.pieces[(x, y)] = (Checker(k, "black", x, y))
                     k += 1
-
         
     def  set_red_pieces(self):
         i = 0
@@ -28,9 +29,13 @@ class Board:
         for i in range(3):
             for j in range(4):
                 if i == 0 or i == 2:
-                    self.pieces.append(Checker(k, "red", 0+(2*j), 2-i))
+                    x = (2*j)
+                    y = 5+i
+                    self.pieces[(x, y)] = (Checker(k, "red", x, y))
                     k += 1
                 if i == 1:
-                    self.pieces.append(Checker(k, "red", 1+(2*j), 2-i))
+                    x = 1+(2*j)
+                    Y = 5+i
+                    self.pieces[(x, y)] = (Checker(k, "red", x, y))
                     k += 1
 
