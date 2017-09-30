499723c92dfb8856dc32d076518586896492e18e
Luke Hardin <luke.hardin@mavs.uta.edu>
2017-09-30 16:53:58 -0500

    added board.py

---
+++ board.py
@@ -0,0 +1,32 @@
+import checker
+
+class Board:
+    def __init__(self):
+        self.pieces = []
+
+    def set_black_pieces(self):
+        i = 0
+        j = 0
+        k = 0
+        for i in range(3):
+            for j in range(4):
+                if i == 0 or i == 2:
+                    self.pieces.append(Checker(k, "black", 1+(2*j), 7-i)
+                    k += 1
+                if i == 1:
+                    self.pieces.append(Checker(k, "black", (2*j), 7-i)
+                    k += 1
+
+        
+    def  set_red_pieces(self):
+        i = 0
+        j = 0
+        k = 0
+        for i in range(3):
+            for j in range(4):
+                if i == 0 or i == 2:
+                    self.pieces.append(Checker(k, "red", 1+(2*j), 2-i)
+                    k += 1
+                if i == 1:
+                    self.pieces.append(Checker(k, "red", (2*j), 2-i)
+                    k += 1
