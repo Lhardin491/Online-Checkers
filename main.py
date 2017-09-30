c89a4623e924056e7665d69a1a5cdb8c906ca258
Elias Jester <jestereli@gmail.com>
2017-09-30 13:53:23 -0500

    Set up simple server

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    with app.open_resource('recourses/HTML') as f:
+        index = f.read()
+        return index
+
+with app.test_request_context():
+    url_for('static', filename='checkers.css')
