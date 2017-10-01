647efd9de9ce72a0fbbf3c4d217c29fa818dfbd2
Luke Hardin <luke.hardin@mavs.uta.edu>
2017-09-30 20:23:00 -0500

    idjsakldjaskl

--- board.py
+++ board.py
@@ -38,3 +38,26 @@ class Board:
                     y = 2-i
                     self.pieces[(x, y)] = (Checker(k, "red", x, y))
                     k += 1
+
+    def get_valid_moves(self, x, y):
+        coordinates = []
+        if self.pieces.get((x, y)) is not None:
+            if self.pieces[(x, y)].king == True:
+                coordinates = [(x+1, y+1), (x+1, y-1), (x+1, y-1),  (x-1, y+1)]
+                for i in coordinates:
+                    if self.pieces[i] is not None:
+                            coordinates.append(
+                    if i[0] > 7 or i[1] > 7 or i[0] < 0 or i[1] < 0:
+                        coordinates.remove(i)
+            else:
+                if self.pieces[(x,y)].color == "black":
+                    coordinates = [(x-1, y-1), (x+1, y-1)]
+                    for i in coordinates:
+                         if i[0] > 7 or i[1] > 7 or i[0] < 0 or i[1] < 0:
+                            coordinates.remove(i)
+                elif self.pieces[(x, y)].color == "red":
+                    coordinates = [(x-1, y+1), (x+1, y+1)]
+                    for i in coordinates:
+                         if i[0] > 7 or i[1] > 7 or i[0] < 0 or i[1] < 0:
+                            coordinates.remove(i)
+        return coordinates
--- test_board.py
+++ test_board.py
@@ -4,4 +4,7 @@ from board import *
 board = Board()
 board.set_red_pieces()
 board.set_black_pieces()
+for i in range(8):
+    for j in range(8):
+        print(board.get_valid_moves(i, j))
 print(board.pieces)
